b045373808c240babbaea7e29a5f30bc6cb14b52
mcv-s <benjamin.pilcher@outlook.com>
2026-08-07 15:36:43 -0400

    Adjust styling, change json file name

--- storage.js
+++ storage.js
@@ -132,7 +132,7 @@
 
 
 
-  function exportData(filename = "folio-backup.json") {
+  function exportData(filename = "folio-settings.json") {
     const finish = (data) => {
       const blob = new Blob(
         [JSON.stringify(data, null, 2)],
